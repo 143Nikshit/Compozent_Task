292b6f0d60b8bf42444f0c30ca81c2162e2b57ad
Nikshit_Joshi <nikshit.g.joshi>
2025-01-07 15:47:21 +0530

    C:\Users\Nikshit.G.Joshi\Desktop\Compozent_Task

---
+++ Machine Learning/app.py
@@ -0,0 +1,37 @@
+from flask import Flask, request, jsonify, render_template
+import joblib
+import pandas as pd
+import numpy as np
+
+app = Flask(__name__)
+
+# Load the model using joblib
+model = joblib.load('car_model.pkl')
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    data = request.json
+
+    # Extracting the input data from the incoming JSON request
+    car = data['Car']
+    model_name = data['Model']
+    volume = data['Volume']
+    weight = data['Weight']
+    co2 = data['CO2']
+
+    # Prepare the feature vector for prediction
+    feature_vector = [[volume, weight, co2]]
+
+    # Making the prediction
+    prediction = model.predict(feature_vector)
+
+    # Return the prediction as a JSON response
+    return jsonify({'predicted_price': prediction[0]})
+
+
+@app.route('/')
+def home_page():
+    return render_template("index.html")  # Render the HTML file
+
+if __name__ == "__main__":
+    app.run(debug=True, port=8000)  # Run the app on port 8000
